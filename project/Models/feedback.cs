5afa2bea9cc79a089c00275e81e0aabf6fc3f545
Soumya E Haridas <soumyaeharidas123@gmail.com>
2020-09-14 22:56:48 +0100

    feedback

---
+++ project/Models/feedback.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.schema;
+
+
+
+public class feedback
+{
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+	public int ID { get; set; }
+	public int? Answer { get; set; }
+	[StringLength(500)]
+	public string Comment { get; set; }
+	[StringLength(100)]
+	public string FullName { get; set; }
+	[StringLength(255)]
+	public string Email { get; set; }
+   
+}
+
+
+
+
+
+	
+   
+
